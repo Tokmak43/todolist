ecc42375f33895c5cd675797569bfc5162ee414b
Alim Tokmakov <tokmak43@mail.ru>
2023-11-06 14:01:13 +0300

    TodoList

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="todolist">
+    <div class="todo-header">
+      <h1 class="title">Список задач</h1>
+      <input class="todo-inputtype" tipe="text" placeholder="укажите задачу"/>
+      <button class="btn">+</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  //name: 'App',
+  date(){
+    return{
+      title:"Разработка приложения TodoList",
+      number:0,
+    };
+  },
+  methods:{
+    addNumber(){
+
+    },
+  },
+  // components: {
+  //   HelloWorld
+  // }
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scope>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+body{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(#21b8ba,#0ad077);
+  background-repeat: no-repeat;
+}
+#app{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 100px;
+  margin-top: 75px;
+}
+.title{
+  margin-top: 20px;
+  font-size: 32px;
+  text-align: center;
+  margin-bottom: 30px;
+}
+.todo-inputtype{
+  padding: 10px 20px;
+  border: 1px solid #ccc;
+  border-radius: 20px;
+  width: 300px;
+}
+.btn{
+  font-size: 40px;
+  padding: 10px;
+  border-radius: 50%;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+createApp(App).mount('#app');
